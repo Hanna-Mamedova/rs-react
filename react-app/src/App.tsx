484df9548dbb4bd7bd27f8c245a6ce997fa85da2
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-18 13:58:10 +0100

    fix: fix app router with layout

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,27 @@
-// import { useState } from 'react';
-import React from 'react';
+import { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import About from './components/about/About';
+import Layout from './components/layout/layout';
+import Main from './components/main/Main';
+import NotFound from './components/not-found/NotFound';
 
 import './App.css';
 
-import Header from './components/header/Header';
-
-function App({ children }) {
-  // const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <Header />
-      {children}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<About />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './index.css';
 
 import App from './App';
-import Main from './components/main/Main';
-import About from './components/about/About';
-import NotFound from './components/not-found/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </App>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
