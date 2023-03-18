23e092c90856ede1b9f8062c86b12cdf173ff6d9
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-18 15:21:37 +0100

    feat: add style to not-found page

--- react-app/src/components/not-found/NotFound.tsx
+++ react-app/src/components/not-found/NotFound.tsx
@@ -1,8 +1,31 @@
 import { Component, ReactNode } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import astronaut from '../../assets/astronaut.svg';
+import planet from '../../assets/planet.svg';
+
+import './not-found.css';
 
 class NotFound extends Component {
   render(): ReactNode {
-    return <div>NotFound</div>;
+    return (
+      <div className="permission_denied">
+        <div id="tsparticles"></div>
+        <div className="denied__wrapper">
+          <h1>404</h1>
+          <h3>
+            LOST IN <span>SPACE</span> App-Name? Hmm, looks like that page does not exist.
+          </h3>
+          <img id="astronaut" src={astronaut} />
+          <img id="planet" src={planet} />
+          <a href="#">
+            <NavLink to="/">
+              <button className="denied__link">Go Home</button>
+            </NavLink>
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
