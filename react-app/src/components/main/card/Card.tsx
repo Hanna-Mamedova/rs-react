0822060cb9002eade45a2bfe8b13bb4c4e17fd8b
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-19 01:14:10 +0100

    feat: add card component and styles

---
+++ react-app/src/components/main/card/Card.tsx
@@ -0,0 +1,47 @@
+import { Book } from 'models/card.model';
+import { Component, ReactNode } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './card.css';
+
+type CardProps = {
+  key: number;
+  book: Book;
+};
+
+type MyState = { book: Book };
+
+class Card extends Component<CardProps, MyState> {
+  constructor(props: CardProps) {
+    super(props);
+
+    this.state = {
+      book: this.props.book,
+    };
+  }
+
+  render(): ReactNode {
+    return (
+      <div className="book-card">
+        <div className="book">
+          <img className="book__image" src={this.props.book.cover_url} alt="" />
+          <h3 className="book__title">{this.props.book.title}</h3>
+          <div>
+            <p className="book__author">{this.props.book.author}</p>
+            <p className="book__price">
+              {this.props.book.price.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              })}
+            </p>
+          </div>
+        </div>
+        <div className="button button-bord">
+          <NavLink to="#">View more</NavLink>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ react-app/src/models/card.model.tsx
@@ -0,0 +1,10 @@
+export interface Book {
+  id: number;
+  price: number;
+  title: string;
+  author: string;
+  cover_url: string;
+  genre: string;
+  language: string;
+  created_date: string;
+}
