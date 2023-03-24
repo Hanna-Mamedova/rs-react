7e5c8bfec6992b54f9b6beb3f595a0a15130ef50
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-24 15:28:49 +0100

    feat: add title, author, price, date inputs

--- src/components/add-book-form/AddBookFormContainer.tsx
+++ src/components/add-book-form/AddBookFormContainer.tsx
@@ -1,9 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import AddBookForm from './AddBookForm';
 
 class AddBookFormContainer extends Component {
+  titleRef = createRef<HTMLInputElement>();
+  authorRef = createRef<HTMLInputElement>();
+  priceRef = createRef<HTMLInputElement>();
+  dateRef = createRef<HTMLInputElement>();
+
+  handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+    console.log('title', this.titleRef.current?.value);
+    console.log('auth', this.authorRef.current?.value);
+    console.log('price', this.priceRef.current?.value);
+    console.log('date', this.dateRef.current?.value);
+  }
+
   render(): JSX.Element {
-    return <AddBookForm />;
+    return (
+      <AddBookForm
+        titleRef={this.titleRef}
+        authorRef={this.authorRef}
+        priceRef={this.priceRef}
+        dateRef={this.dateRef}
+        handleSubmit={(e) => this.handleSubmit(e)}
+      />
+    );
   }
 }
 
