9d323e6c1283e6f5572c66ac3b28f9e4dc262434
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-25 21:55:12 +0100

    fix: fix cards

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Cards from '../../components/main/cards/Cards';
 import SearchBar from '../../components/main/search-bar/SearchBar';
 
+import { books } from '../../components/main/cards/cards-data';
+
 class Main extends Component {
   render(): React.ReactNode {
     return (
       <div>
         <SearchBar />
-        <Cards />
+        <Cards books={books} />
       </div>
     );
   }
