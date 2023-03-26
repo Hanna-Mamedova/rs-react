b1ad6af86807c9b0ff17c60b88912737ec2648b4
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-26 22:59:19 +0200

    feat: add validation types

---
+++ src/components/add-book-form/validation/ValidationTypes.ts
@@ -0,0 +1,41 @@
+export interface Inputs {
+  title: {
+    required: boolean;
+    errMsg: string;
+    min: number;
+    max: number;
+  };
+  author: {
+    required: boolean;
+    errMsg: string;
+  };
+  price: {
+    required: boolean;
+    errMsg: string;
+  };
+  date: {
+    required: boolean;
+    errMsg: string;
+    errFutureDate: string;
+  };
+  lang: {
+    required: boolean;
+    errMsg: string;
+  };
+  genre: {
+    required: boolean;
+    errMsg: string;
+  };
+  onStock: {
+    required: boolean;
+    errMsg: string;
+  };
+  image: {
+    required: boolean;
+    errMsg: string;
+  };
+}
+
+export interface InputsReqs extends Inputs {
+  error: boolean;
+}
