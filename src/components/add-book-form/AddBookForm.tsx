50ccac01e3d6c0c829de113f1639596ffc529ce7
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-26 23:00:44 +0200

    feat: add errors spans to form

--- src/components/add-book-form/AddBookForm.tsx
+++ src/components/add-book-form/AddBookForm.tsx
@@ -6,6 +6,7 @@ import Dropdown from './components/dropdown/Dropdown';
 import CheckboxList from './components/checkbox-list/CheckboxList';
 import Switcher from './components/switcher/Switcher';
 import TextInput from './components/text-input/TextInput';
+import { InputsReqs } from './validation/ValidationTypes';
 
 export type RefsType = {
   [key: number]: HTMLInputElement | null;
@@ -18,6 +19,7 @@ interface AddBookProps {
   dateRef: React.RefObject<HTMLInputElement>;
   dropdownRef: React.RefObject<HTMLSelectElement>;
   imageRef: React.RefObject<HTMLInputElement>;
+  validReqs: InputsReqs;
   handleSubmit: (
     e: React.FormEvent<HTMLFormElement>,
     checkboxes: RefsType,
@@ -45,13 +47,23 @@ class AddBookForm extends Component<AddBookProps> {
           className="form-container"
         >
           <TextInput name="title" type="text" innerRef={this.props.titleRef} />
+          <span style={{ display: 'none' }} className="error">
+            {this.props.validReqs.title.errMsg}
+          </span>
           <TextInput name="author" type="text" innerRef={this.props.authorRef} />
+          <span></span>
           <TextInput name="price" type="number" innerRef={this.props.priceRef} />
+          <span></span>
           <TextInput name="date" type="date" innerRef={this.props.dateRef} />
+          <span></span>
           <Dropdown innerRef={this.props.dropdownRef} />
+          <span></span>
           <CheckboxList onCheckboxClick={(refs) => this.getCheckboxes(refs)} />
+          <span></span>
           <Switcher onSwitcherClick={(refs) => this.getSwitcher(refs)} />
+          <span></span>
           <TextInput name="image" type="file" innerRef={this.props.imageRef} />
+          <span></span>
           <div className="btn-submit__container">
             <button className="btn-submit" type="submit">
               Add
