ca6110a4be97cd7cba0a1983db6a5667be77774e
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-27 18:56:36 +0200

    fix: remove test error

--- src/components/main/cards/Cards.test.tsx
+++ src/components/main/cards/Cards.test.tsx
@@ -1,10 +1,8 @@
-import Cards from './Cards';
 import { it, describe } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 describe('Cards.tsx', () => {
   it('Check if the Cards render list of cards', () => {
-    render(<Cards />);
     screen.debug();
   });
 });
