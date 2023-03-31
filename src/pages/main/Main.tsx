613815fa38e99cfe5b531f4632f2099a47284372
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-31 22:22:11 +0200

    feat: rewrite to fc main file

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
 import Cards from '../../components/main/cards/Cards';
 import SearchBar from '../../components/main/search-bar/SearchBar';
 
 import { books } from '../../components/main/cards/cards-data';
 
-class Main extends Component {
-  render(): React.ReactNode {
-    return (
-      <div>
-        <SearchBar />
-        <Cards books={books} />
-      </div>
-    );
-  }
+function Main() {
+  return (
+    <div>
+      <SearchBar />
+      <Cards books={books} />
+    </div>
+  );
 }
 
 export default Main;
