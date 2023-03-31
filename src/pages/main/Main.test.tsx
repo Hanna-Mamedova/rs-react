b9e240c95c404f16cb52a29f6f7f3a5b48aef95e
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-03-31 22:55:42 +0200

    feat: add test to main

--- src/pages/main/Main.test.tsx
+++ src/pages/main/Main.test.tsx
@@ -1,10 +1,17 @@
 import Main from './Main';
 import { it, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { Book } from 'models/card.model';
 
-describe('Main.tsx', () => {
-  it('Check if the App render very well', () => {
+describe('Main', () => {
+  it('Check if the Main renders', () => {
     render(<Main />);
+    const searchBar = screen.getByRole('search');
+    const cards = screen.getByRole('list');
+
+    expect(searchBar).toBeInTheDocument();
+    expect(cards).toBeInTheDocument();
+
     screen.debug();
   });
 });
