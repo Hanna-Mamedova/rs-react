570d8ecdd9e034ed63a048facc92c3bc7174a7ed
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-04-02 18:55:31 +0200

    refactor: change about to fc

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,22 +1,18 @@
-import { Component, ReactNode } from 'react';
-
 import './About.css';
 
-class About extends Component {
-  render(): ReactNode {
-    return (
-      <div className="about">
-        <h1>About</h1>
-        <div className="about-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magnam, quia eius sunt
-            cumque dignissimos deserunt temporibus distinctio eos rem quis. Assumenda voluptatibus
-            neque fugit aliquam beatae fuga saepe cupiditate.
-          </p>
-        </div>
+function About() {
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <div className="about-content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magnam, quia eius sunt
+          cumque dignissimos deserunt temporibus distinctio eos rem quis. Assumenda voluptatibus
+          neque fugit aliquam beatae fuga saepe cupiditate.
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default About;
