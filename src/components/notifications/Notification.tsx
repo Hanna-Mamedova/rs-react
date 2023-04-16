9e780e5e3c50f13683b0e9f0897aa334c7ff4ddd
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-04-16 11:53:05 +0200

    refactor: refactor notification component

--- src/components/notifications/Notification.tsx
+++ src/components/notifications/Notification.tsx
@@ -11,23 +11,24 @@ type NotificationProps = {
 };
 
 function Notification(props: NotificationProps) {
-  if (props.notificationVisible) {
-    return (
-      <div className="notification-container top-right">
-        <div className="notification toast top-right">
-          <button onClick={props.onClose}>X</button>
-          <div className="notification-image">
-            <img src={checkIcon} alt="success-icon" />
-          </div>
-          <div>
-            <p className="notification-title">{props.toast.title}</p>
-            <p className="notification-message">{props.toast.description}</p>
+  return (
+    <>
+      {props.notificationVisible ? (
+        <div className="notification-container top-right">
+          <div className="notification toast top-right">
+            <button onClick={props.onClose}>X</button>
+            <div className="notification-image">
+              <img src={checkIcon} alt="success-icon" />
+            </div>
+            <div>
+              <p className="notification-title">{props.toast.title}</p>
+              <p className="notification-message">{props.toast.description}</p>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  }
-  return <></>;
+      ) : null}
+    </>
+  );
 }
 
 export default Notification;
