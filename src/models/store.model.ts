c0e790bba236e7e5c694d43e47129aacf4fb2ea5
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-04-16 13:15:55 +0200

    refactor: change necessary files from tsx to ts

--- src/models/store.model.ts
+++ src/models/store.model.ts
@@ -1,6 +1,10 @@
 import { CharactersState } from '../store/reducers/charactersSlice';
+import { FormValues } from './form.model';
+import { NewBook } from './card.model';
 
 export default interface AppState {
   searchText: '';
   characters: CharactersState;
+  addBookForm: FormValues;
+  newBooks: NewBook[];
 }
