a73184c0483a670675fe20ee359a84f9d278386a
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-04-29 23:43:46 +0200

    feat: add smth to server

--- server.ts
+++ server.ts
@@ -8,6 +8,26 @@ import store from './src/store/store';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PORT = 5173;
 
+function renderFullPage(html, preloadedState) {
+  return `
+  <!doctype html>
+  <html>
+    <head>
+      <title>Redux Universal Example</title>
+    </head>
+    <body>
+      <div id="root">${html}</div>
+      <script>
+        // WARNING: See the following for security issues around embedding JSON in HTML:
+        // https://redux.js.org/usage/server-rendering#security-considerations
+        window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+      </script>
+      <script src="/static/bundle.js"></script>
+    </body>
+  </html>
+  `;
+}
+
 async function createServer() {
   const app = express();
   const vite = await createViteServer({
@@ -15,7 +35,7 @@ async function createServer() {
     appType: 'custom',
   });
   app.use(vite.middlewares);
-  app.use('/static', Express.static('static'));
+  app.use('/static', express.static('static'));
   app.use('*', async (req, res, next) => {
     const url = req.originalUrl;
 
@@ -23,21 +43,16 @@ async function createServer() {
       let template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
       template = await vite.transformIndexHtml(url, template);
       const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
-
-      const preloadedState = store.getState();
-      res.send(renderFullPage(html, preloadedState));
-
       const appHtml = await render(url);
       const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+      const preloadedState = store.getState();
+      res.send(renderFullPage(html, preloadedState));
       res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
     } catch (e) {
       vite.ssrFixStacktrace(e);
       next(e);
     }
   });
-  function renderFullPage(html, preloadedState) {
-    /* ... */
-  }
   app.listen(PORT);
 }
 
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -3,18 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
+import { Provider } from 'react-redux';
+import store from 'store/store';
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
